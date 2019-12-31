4f9ca853d6b7afe4a7a7c200c5f31be53d9f8c9a
JenyaIII-sudo <evgeniyfirm.25@gmail.com>
2019-12-31 12:43:36 +0300

    config connecting to heroku postgresQL

--- config/database.js
+++ config/database.js
@@ -5,13 +5,14 @@ const dbUser = process.env.DB_USER;
 const dbUserPass = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 
-module.exports = new Sequelize(dataBase, dbUser, dbUserPass, {
-  host: dbHost,
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+// module.exports = new Sequelize(dataBase, dbUser, dbUserPass, {
+//   host: dbHost,
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// });
+module.exports = new Sequelize(process.env.DATABASE_URI);
